1708c52c86791bda711c097b3eda1112cf1ee5b1
fakod <fakod@9f3fa172-c50d-0410-b941-ca79000a7999>
2006-04-03 19:28:26 +0000

    added annotation to implementation

--- JDBCluster/example/mycluster/service/CBicycleService.java
+++ JDBCluster/example/mycluster/service/CBicycleService.java
@@ -2,11 +2,14 @@ package mycluster.service;
 
 import mycluster.CBicycle;
 
+import org.jdbcluster.metapersistence.annotation.PrivilegesMethod;
+import org.jdbcluster.metapersistence.annotation.PrivilegesService;
 import org.jdbcluster.service.ServiceBase;
 
-
+@PrivilegesService(required="BIKESERVCIE")
 public class CBicycleService extends ServiceBase implements ICBicycleService {
 
+	@PrivilegesMethod(required="BIKEPAINT")
 	public void makeBicycleRED(CBicycle bike) {
 	}
 
