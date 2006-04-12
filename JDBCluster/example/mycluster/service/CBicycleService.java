c4512f38838cafa674d0d5417d2387d1d05445ec
fakod <fakod@9f3fa172-c50d-0410-b941-ca79000a7999>
2006-04-12 18:15:37 +0000

    changed to privileged service

--- JDBCluster/example/mycluster/service/CBicycleService.java
+++ JDBCluster/example/mycluster/service/CBicycleService.java
@@ -4,14 +4,14 @@ import mycluster.CBicycle;
 
 import org.jdbcluster.metapersistence.annotation.PrivilegesMethod;
 import org.jdbcluster.metapersistence.annotation.PrivilegesService;
+import org.jdbcluster.service.PrivilegedService;
 import org.jdbcluster.service.ServiceBase;
 
 @PrivilegesService(required="BIKESERVCIE")
-public class CBicycleService extends ServiceBase implements ICBicycleService {
+public class CBicycleService extends ServiceBase implements ICBicycleService, PrivilegedService {
 
 	@PrivilegesMethod(required="BIKEPAINT")
 	public void makeBicycleRED(CBicycle bike) {
 	}
 
-	
 }
