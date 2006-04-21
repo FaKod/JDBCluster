1369ebfecad07ef9e73d438e863c59ece14967da
fakod <fakod@9f3fa172-c50d-0410-b941-ca79000a7999>
2006-04-21 14:44:52 +0000

    added test for nested properties for privileges

--- JDBCluster/test/test/privilege/TestPrivilege.java
+++ JDBCluster/test/test/privilege/TestPrivilege.java
@@ -1,7 +1,10 @@
 package test.privilege;
 
+import java.util.Set;
+
 import mycluster.CBicycle;
 import mycluster.NestedPropertyForPrivTest;
+import mycluster.privilege.UserPrivilege;
 
 import org.apache.log4j.PropertyConfigurator;
 import org.jdbcluster.JDBClusterSimpleConfig;
@@ -29,22 +32,113 @@ public class TestPrivilege extends TestCase {
 
 		// create a Cluster and persist it
 		CBicycle rad = ClusterFactory.newInstance(cFahrradType);
-
-		/**
-		 * 
-		 */
 		
-		rad.setNested(new NestedPropertyForPrivTest());
+		Set<String> lastPrivSet;
+
+		
+		/**
+		 * needed rights for setColorType
+		 * this is a domain without dependancies
+		 */
 		
 		rad.setColorType("Color");
 		
+		lastPrivSet = UserPrivilege.getPrivSet();
+		assertTrue(lastPrivSet.size()==2);
+		assertTrue(lastPrivSet.contains("BIKE"));
+		assertTrue(lastPrivSet.contains("GETNESTED"));
+		
+		UserPrivilege.clearLastprivSet();
+		
+		/**
+		 * needed rights for setColor
+		 */
+		
 		rad.setColor("RED");
 		
+		lastPrivSet = UserPrivilege.getPrivSet();
+		assertTrue(lastPrivSet.size()==6);
+		assertTrue(lastPrivSet.contains("BIKE"));
+		assertTrue(lastPrivSet.contains("GETCOLTYPE"));
+		assertTrue(lastPrivSet.contains("GETNESTED"));
+		assertTrue(lastPrivSet.contains("PRIV1"));
+		assertTrue(lastPrivSet.contains("PRIV2"));
+		assertTrue(lastPrivSet.contains("PRIV3"));
+		
+		UserPrivilege.clearLastprivSet();
+				
+		/**
+		 * needed rights for setColorShading
+		 */
+		
 		rad.setColorShading("LightRED");
 		
+		lastPrivSet = UserPrivilege.getPrivSet();
+		assertTrue(lastPrivSet.size()==8);
+		assertTrue(lastPrivSet.contains("BIKE"));
+		assertTrue(lastPrivSet.contains("GETCOLTYPE"));
+		assertTrue(lastPrivSet.contains("GETNESTED"));
+		assertTrue(lastPrivSet.contains("SETCOLSHADE"));
+		assertTrue(lastPrivSet.contains("GETCOLOR"));
+		assertTrue(lastPrivSet.contains("PRIV1"));
+		assertTrue(lastPrivSet.contains("PRIV2"));
+		assertTrue(lastPrivSet.contains("PRIV3"));
 		
+		UserPrivilege.clearLastprivSet();
+		
+		/**
+		 * needed rights for setNested
+		 */
+		
+		rad.setNested(new NestedPropertyForPrivTest());
+				
+		lastPrivSet = UserPrivilege.getPrivSet();
+		assertTrue(lastPrivSet.size()==6);
+		assertTrue(lastPrivSet.contains("BIKE"));
+		assertTrue(lastPrivSet.contains("GETNESTED"));
+		assertTrue(lastPrivSet.contains("GETCOLTYPE"));
+		assertTrue(lastPrivSet.contains("PRIV1"));
+		assertTrue(lastPrivSet.contains("PRIV2"));
+		assertTrue(lastPrivSet.contains("PRIV3"));
+		
+		UserPrivilege.clearLastprivSet();
+				
+		/**
+		 * needed rights for getNested
+		 */
+		
+		NestedPropertyForPrivTest n = rad.getNested();
+
+		lastPrivSet = UserPrivilege.getPrivSet();
+		assertTrue(lastPrivSet.size()==2);
+		assertTrue(lastPrivSet.contains("BIKE"));
+		assertTrue(lastPrivSet.contains("GETNESTED"));
+		
+		UserPrivilege.clearLastprivSet();
+		
+		n.setNestedDomain("TEST");
+		
+		/**
+		 * needed rights for setColorShading
+		 */
 		
 		rad.setColorShading("LightRED");
+		
+		lastPrivSet = UserPrivilege.getPrivSet();
+		assertTrue(lastPrivSet.size()==11);
+		assertTrue(lastPrivSet.contains("BIKE"));
+		assertTrue(lastPrivSet.contains("GETCOLTYPE"));
+		assertTrue(lastPrivSet.contains("GETNESTED"));
+		assertTrue(lastPrivSet.contains("SETCOLSHADE"));
+		assertTrue(lastPrivSet.contains("GETCOLOR"));
+		assertTrue(lastPrivSet.contains("PRIV1"));
+		assertTrue(lastPrivSet.contains("PRIV2"));
+		assertTrue(lastPrivSet.contains("PRIV3"));
+		assertTrue(lastPrivSet.contains("NestedPRIV1"));
+		assertTrue(lastPrivSet.contains("NestedPRIV2"));
+		assertTrue(lastPrivSet.contains("NestedPRIV3"));
+		
+		UserPrivilege.clearLastprivSet();
 		
 	}
 }
