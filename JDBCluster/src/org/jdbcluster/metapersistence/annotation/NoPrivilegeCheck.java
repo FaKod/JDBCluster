5a9dd2fdcc26122bff7880d4a1160dc1416ba21d
fakod <fakod@9f3fa172-c50d-0410-b941-ca79000a7999>
2006-04-25 19:34:41 +0000

    for PARAMETER too

--- JDBCluster/src/org/jdbcluster/metapersistence/annotation/NoPrivilegeCheck.java
+++ JDBCluster/src/org/jdbcluster/metapersistence/annotation/NoPrivilegeCheck.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD})
+@Target({ElementType.METHOD, ElementType.PARAMETER})
 public @interface NoPrivilegeCheck {
 
 }
