8bf26fd22d3ff23f7fd127fed04ad6b04c23cb45
fakod <fakod@9f3fa172-c50d-0410-b941-ca79000a7999>
2006-05-03 18:40:17 +0000

    enhanced service test

--- JDBCluster/test/test/service/TestService.java
+++ JDBCluster/test/test/service/TestService.java
@@ -1,6 +1,9 @@
 package test.service;
 
+import java.util.Set;
+
 import mycluster.CBicycle;
+import mycluster.privilege.UserPrivilege;
 import mycluster.service.CBicycleService;
 import mycluster.service.ICBicycleService;
 
@@ -23,13 +26,46 @@ public class TestService extends TestCase {
 	}
 
 	public void testService() {
+		Set<String> lastPrivSet;
+		ICBicycleService bs = new CBicycleService();
+		
 //		 we use cluster auto now
 		ClusterType cFahrradType = ClusterTypeFactory.newInstance("bicycle");
 
 		// create a Cluster and persist it
 		CBicycle rad = ClusterFactory.newInstance(cFahrradType);
 		
-		ICBicycleService bs = new CBicycleService();
+		/*
+		 * check an empty CBicycle object
+		 */
+		UserPrivilege.clearLastprivSet();
+		
 		bs.makeBicycleRED(rad);
+		
+		lastPrivSet = UserPrivilege.getPrivSet();
+		assertTrue(lastPrivSet.size()==5);
+		assertTrue(lastPrivSet.contains("BIKE"));
+		assertTrue(lastPrivSet.contains("GETCOLOR"));
+		assertTrue(lastPrivSet.contains("NeedsRight4[Color]DomainValue[NULL]"));
+		assertTrue(lastPrivSet.contains("BIKESERVCIE"));
+		assertTrue(lastPrivSet.contains("BIKEPAINT"));
+		
+		/**
+		 * fill with some values
+		 */
+		rad.setColorType("Color");
+		rad.setColor("RED");
+		
+		UserPrivilege.clearLastprivSet();
+		
+		bs.makeBicycleRED(rad);
+		
+		lastPrivSet = UserPrivilege.getPrivSet();
+		assertTrue(lastPrivSet.size()==5);
+		assertTrue(lastPrivSet.contains("BIKE"));
+		assertTrue(lastPrivSet.contains("GETCOLOR"));
+		assertTrue(lastPrivSet.contains("NeedsRight4[Color]DomainValue[RED]"));
+		assertTrue(lastPrivSet.contains("BIKESERVCIE"));
+		assertTrue(lastPrivSet.contains("BIKEPAINT"));
 	}
 }
