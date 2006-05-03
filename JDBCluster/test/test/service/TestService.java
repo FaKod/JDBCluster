7f9d1018b44fd2f753827a33892a3372f569ea72
fakod <fakod@9f3fa172-c50d-0410-b941-ca79000a7999>
2006-05-03 19:09:54 +0000

    enhancements for test

--- JDBCluster/test/test/service/TestService.java
+++ JDBCluster/test/test/service/TestService.java
@@ -11,8 +11,11 @@ import org.apache.log4j.PropertyConfigurator;
 import org.jdbcluster.JDBClusterSimpleConfig;
 import org.jdbcluster.clustertype.ClusterType;
 import org.jdbcluster.clustertype.ClusterTypeFactory;
+import org.jdbcluster.exception.ConfigurationException;
+import org.jdbcluster.metapersistence.annotation.PrivilegesParameter;
 import org.jdbcluster.metapersistence.cluster.ClusterFactory;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class TestService extends TestCase {
@@ -67,5 +70,30 @@ public class TestService extends TestCase {
 		assertTrue(lastPrivSet.contains("NeedsRight4[Color]DomainValue[RED]"));
 		assertTrue(lastPrivSet.contains("BIKESERVCIE"));
 		assertTrue(lastPrivSet.contains("BIKEPAINT"));
+		
+		/**
+		 * check for colorType property check
+		 * see @PrivilegesParameter(property={"colorType"}
+		 */
+		UserPrivilege.clearLastprivSet();
+		bs.makeBicycleCheckParam(rad);
+		
+		lastPrivSet = UserPrivilege.getPrivSet();
+		assertTrue(lastPrivSet.size()==5);
+		assertTrue(lastPrivSet.contains("BIKE"));
+		assertTrue(lastPrivSet.contains("GETCOLTYPE"));
+		assertTrue(lastPrivSet.contains("BIKEPAINT_CP"));
+		assertTrue(lastPrivSet.contains("BIKESERVCIE"));
+		assertTrue(lastPrivSet.contains("NeedsRight4[ColorType]DomainValue[Color]"));
+		
+		/**
+		 * test for unallowed property (must be a privileged domain)
+		 * see @PrivilegesParameter(property={"colorShading"}
+		 */
+		try {
+			bs.makeBicycleCheckParam2(rad);
+			Assert.fail("usage of non privileged domain colorShading is not allowed");
+		} catch (ConfigurationException e) {
+		}
 	}
 }
