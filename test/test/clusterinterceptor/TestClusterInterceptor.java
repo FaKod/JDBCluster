59c8923096540f9c2312e199f4ca42e962368e37
fakod <fakod@9f3fa172-c50d-0410-b941-ca79000a7999>
2006-05-10 13:39:54 +0000

    ClusterInterceptor Concept: added test

---
+++ test/test/clusterinterceptor/TestClusterInterceptor.java
@@ -0,0 +1,45 @@
+package test.clusterinterceptor;
+
+import mycluster.CBicycle;
+import mycluster.CCar;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.jdbcluster.JDBClusterSimpleConfig;
+import org.jdbcluster.clustertype.ClusterType;
+import org.jdbcluster.clustertype.ClusterTypeFactory;
+import org.jdbcluster.exception.ConfigurationException;
+import org.jdbcluster.metapersistence.cluster.ClusterFactory;
+
+import junit.framework.TestCase;
+
+public class TestClusterInterceptor extends TestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		// configuring logging
+		PropertyConfigurator.configure("xml/logging.properties");
+
+		JDBClusterSimpleConfig
+				.setConfiguration("test/test/clusterinterceptor/jdbcluster.conf.ClusterInterceptor.xml");
+		super.setUp();
+	}
+
+	public void testDomainOneDep() {
+		// we use cluster auto now
+		ClusterType cFahrradType = ClusterTypeFactory.newInstance("bicycle");
+
+		// create a Cluster and persist it
+		CBicycle rad = ClusterFactory.newInstance(cFahrradType);
+
+		// we use cluster auto now
+		ClusterType cAutoType = ClusterTypeFactory.newInstance("car");
+
+		// create a Cluster and persist it
+		// but interceptor returning false -> exception
+		try {
+			CCar bmw = ClusterFactory.newInstance(cAutoType);
+		} catch (ConfigurationException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
