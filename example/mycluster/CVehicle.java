952b1f416cb403d79cfc421d1d5b6b269f7bd3c7
fakod <fakod@9f3fa172-c50d-0410-b941-ca79000a7999>
2006-06-26 06:14:59 +0000

    warning for Cluster subclassing without @DaoLink

--- example/mycluster/CVehicle.java
+++ example/mycluster/CVehicle.java
@@ -1,11 +1,7 @@
 package mycluster;
 
-import org.jdbcluster.metapersistence.annotation.DaoLink;
 import org.jdbcluster.metapersistence.cluster.Cluster;
 
-import dao.Vehicle;
-
-@DaoLink(dAOClass = Vehicle.class)
 public abstract class CVehicle extends Cluster {
 
 	long id;
