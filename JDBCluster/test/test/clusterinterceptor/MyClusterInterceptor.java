5bd58a8f15638ff51571c648b568639854de0468
fakod <fakod@9f3fa172-c50d-0410-b941-ca79000a7999>
2008-02-18 19:48:37 +0000

    due to interface usage

--- JDBCluster/test/test/clusterinterceptor/MyClusterInterceptor.java
+++ JDBCluster/test/test/clusterinterceptor/MyClusterInterceptor.java
@@ -2,12 +2,12 @@ package test.clusterinterceptor;
 
 import mycluster.CBicycle;
 
-import org.jdbcluster.metapersistence.cluster.Cluster;
 import org.jdbcluster.metapersistence.cluster.ClusterInterceptor;
+import org.jdbcluster.metapersistence.cluster.ICluster;
 
 public class MyClusterInterceptor implements ClusterInterceptor {
 
-	public boolean clusterNew(Cluster cluster) {
+	public boolean clusterNew(ICluster cluster) {
 		if(cluster instanceof CBicycle) {
 			CBicycle b = (CBicycle) cluster;
 			b.setDurchmesser(123);
@@ -16,7 +16,7 @@ public class MyClusterInterceptor implements ClusterInterceptor {
 		return false;
 	}
 
-	public boolean clusterRefresh(Cluster cluster) {
+	public boolean clusterRefresh(ICluster cluster) {
 		return true;
 	}
 
