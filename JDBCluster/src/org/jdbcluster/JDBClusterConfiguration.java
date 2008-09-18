ff21540bdf5b127902e89eafb6a48c363d89d463
phil82 <phil82@9f3fa172-c50d-0410-b941-ca79000a7999>
2008-09-18 13:38:10 +0000

    added javadoc

--- JDBCluster/src/org/jdbcluster/JDBClusterConfiguration.java
+++ JDBCluster/src/org/jdbcluster/JDBClusterConfiguration.java
@@ -2,5 +2,13 @@ package org.jdbcluster;
 
 public interface JDBClusterConfiguration {
 
+	/**
+	 * This method extends the content of jdbcluster.conf.xml file with the content
+	 * provided by the parameter.
+	 * @param extensionPath the path to the extension file. The content of this file is merged with the 
+	 * base configuration file. If new filter elements are defined for an existing cluster those elements
+	 * are added. If the new filter elements are already specified in the jdbcluster.conf.xml they are overridden
+	 * with the new values specified within the extension file.
+	 */
 	public void addExtension(String extensionPath);
 }
